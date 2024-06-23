868dc60d77737d2b8481390b1b498a39b6a16166
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-06-23 20:06:27 +0300

    Created the sign up and sign in pages

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,78 @@
+import { Image, StyleSheet, Text, View, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
+
+const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMethod="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) =>
+              SemanticClassificationFormat({ ...form, email: e })
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) =>
+              SemanticClassificationFormat({ ...form, password: e })
+            }
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5  flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't Have an account ?
+            </Text>
+
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              {" "}
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
+
+const styles = StyleSheet.create({});
