b2754f25bab093fb428520c7c8f3adbd9ffd1f0d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-06-23 15:04:15 +0300

    Create tab navigation for the app

--- app/index.jsx
+++ app/index.jsx
@@ -5,11 +5,10 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Test app</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
+      <Link href="/home" style={{ color: "blue" }}>
         {" "}
-        Go to Profile{" "}
+        Go to Home{" "}
       </Link>
     </View>
   );
